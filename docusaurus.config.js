8157acff3d8485489c3e7c0a8eb1da23573c1a54
Dagmar Timler <dagmar.timler@gmail.com>
2024-11-13 14:41:50 +0200

    Update config to set github details

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,13 +20,9 @@ const config = {
   tagline: 'Quantification Analytics Tool',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://www.quantificationanalytics.org/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
+  // Github Pages deployment config
+  url: `https://${orgName}.github.io`,
+  baseUrl: `/${projectName}/`,
   organizationName: orgName,
   projectName: projectName,
 
